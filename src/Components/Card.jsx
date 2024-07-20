2803926eaf555cbb052e60da336ac31e0ca047b1
juned072 <junedansari9172@gmail.com>
2024-07-20 18:57:45 +0530

    added Card and applied styled it

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    <div className="shadow-md p-2 rounded-md flex justify-between items-center">
+      <div>
+        <h3 className="text-lg text-gray-800">Mangoes</h3>
+        <h4 className="text-blue-500">$100</h4>
+      </div>
+      <div>
+        <button className="bg-red-500 hover:bg-red-600 text-white rounded-md md:p-2 p-1">
+          Remove
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
