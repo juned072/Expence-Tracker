7f44380fb4c605e5cfb3fe4e722a3d5655646902
juned072 <junedansari9172@gmail.com>
2024-07-20 18:21:16 +0530

    added height , widh and background color

--- src/Components/ContainerBox.jsx
+++ src/Components/ContainerBox.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const ContainerBox = () => {
-  return <div>ContainerBox</div>;
+  return (
+    <div className="bg-white min-h-96 max-h-96 min-w-[550px] max-w-[550px] rounded-md p-5 overflow-y-auto">
+      ContainerBox
+    </div>
+  );
 };
 
 export default ContainerBox;
