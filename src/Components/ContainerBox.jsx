9dd069ceb56a27ab06830f3e193f195bebb70df5
juned072 <junedansari9172@gmail.com>
2024-07-20 18:29:10 +0530

    designed under container added text and button

--- src/Components/ContainerBox.jsx
+++ src/Components/ContainerBox.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 const ContainerBox = () => {
   return (
-    <div className="bg-white min-h-96 max-h-96 min-w-[550px] max-w-[550px] rounded-md p-5 overflow-y-auto">
-      ContainerBox
+    <div className="bg-white min-h-[450px] max-h-[450px] min-w-[550px] max-w-[550px] rounded-md overflow-y-auto relative">
+      <div className="bg-gray-100 absolute bottom-0 left-0 w-full p-4 flex items-center justify-between">
+        <h3 className="font-serif text-xl text-gray-800">
+          Total Price: <span className="text-blue-500 text-3xl">$100</span>
+        </h3>
+        <button className="bg-red-500 hover:bg-red-600 rounded-md text-white p-2">
+          Clear All
+        </button>
+      </div>
     </div>
   );
 };
