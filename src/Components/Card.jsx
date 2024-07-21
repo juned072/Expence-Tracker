fb4eb488788729c9cf906c88f30e3ccf9be4b432
juned072 <junedansari9172@gmail.com>
2024-07-21 09:48:39 +0530

    item are get into card as a props and set all data

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({ item }) => {
   return (
     <div className="shadow-md p-2 rounded-md flex justify-between items-center">
       <div>
-        <h3 className="text-lg text-gray-800">Mangoes</h3>
-        <h4 className="text-blue-500">$100</h4>
+        <h3 className="text-lg text-gray-800">{item.itemName}</h3>
+        <h4 className="text-blue-500">${item.itemPrice}</h4>
       </div>
       <div>
         <button className="bg-red-500 hover:bg-red-600 text-white rounded-md md:p-2 p-1">
